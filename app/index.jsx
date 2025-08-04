4d80a9ba29a201ff4798237ef87a4acfafcb15ca
hafeezrafeeq <hafeezrafeeq031@gmail.com>
2025-08-04 23:05:43 +0500

    Dinner-Time app is start

---
+++ app/index.jsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 items-center justify-center bg-stone-50">
+      <Text className="text-1xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+ 
+  );
+}
