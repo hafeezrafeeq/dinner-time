ad1e15c7782b821ba3abe131d23ab9b2da8f3cb0
hafeezrafeeq <hafeezrafeeq031@gmail.com>
2025-08-07 21:31:33 +0500

    Sign up page complete

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,48 @@
+import { View, Text } from 'react-native'
+import React from 'react';
+import { Tabs } from 'expo-router'
+import { Colors } from '../../assets/Colors'
+import { Ionicons } from '@expo/vector-icons';
+
+
+
+const Tablayout = () => {
+    return (
+        <Tabs screenOptions={{
+            headerShown: false,
+            tabBarActiveTintColor: Colors.PRIMARY,
+            tabBarInActiveTintColor: Colors.dark.text,
+
+            tabBarStyle: {
+                backgroundColor: Colors.SECONDARY,
+                paddingTop: 6,
+                height: 95,
+            },
+            tabBarLabelStyle: {
+                fontSize: 12,
+                fontWeight: "bold"
+            },
+        }}>
+            <Tabs.Screen name="home" options={{
+                title: "Home",
+                tabBarIcon: ({ color }) => (
+                    <Ionicons name='home' size={24} color={color} />
+                ),
+            }} />
+            <Tabs.Screen name="history" options={{
+                title: "History",
+                tabBarIcon: ({ color }) => (
+                    <Ionicons name='time' size={24} color={color} />
+                ),
+            }} />
+            <Tabs.Screen name="profile" options={{
+                title: "Profile",
+                tabBarIcon: ({ color }) => (
+                    <Ionicons name='person-sharp' size={24} color={color} />
+                ),
+            }} />
+        </Tabs>
+    )
+}
+
+export default Tablayout
--- app/index.jsx
+++ app/index.jsx
@@ -1,20 +1,73 @@
 import { useRouter } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Image, ScrollView, StatusBar, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+// import "../global.css";
+// import logo from '../assets/images/dinetimelogo.png';
 
+const frame = require("../assets/images/Frame.png")
+const logo = require("../assets/images/dinetimelogo.png");
 export default function Index() {
   const router = useRouter();
 
   return (
-    <View className="flex-1 items-center justify-center bg-stone-50">
-      <Text className="text-1xl font-bold text-blue-500">
-        hello expo
-      </Text>
+    <SafeAreaView style={{ backgroundColor: "#2b2b2b", flex: 1 }}>
+      <StatusBar barStyle={"light-content"} backgroundColor={"#2b2b2b"} />
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
 
-      <TouchableOpacity onPress={() => router.push("/Login-Screen")}>
-        <Text>
-          Login Page Add
-        </Text>
-      </TouchableOpacity>
-    </View>
+        <View className="m-2 flex flex-col items-center">
+
+          <Image source={logo} alt="image" style={{ width: 300, height: 300 }} />
+
+
+          <View className="w-3/4 ">
+
+            <TouchableOpacity
+              onPress={() => router.push("/signup")}
+              className=" bg-[#f49b33]  px-28 py-3 mt-5 rounded"
+            >
+              <Text className="text-white text-lg font-semibold text-center">
+                Sign Up
+              </Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress={() => router.push("/home")}
+              className="border-2 border-[#f49b33]  px-28 py-3 mt-5 rounded"
+            >
+              <Text className="text-[#F49b33] text-lg font-semibold text-center">
+                Guest User
+              </Text>
+            </TouchableOpacity>
+
+          </View>
+
+
+          <View>
+            <Text className="text-white text-base font-semibold my-4">
+          
+              <View className="border-b-2 border-[#f49b33] w-24" />{" "} or {"  "}
+              <View className="border-b-2 border-[#f49b33] w-24" />
+            </Text>
+
+
+            <TouchableOpacity onPress={() => router.push("/signin")}
+              className="flex flex-row items-center justify-center mt-1 "
+            >
+              <Text className="text-white font-semibold mr-2 ">Already a User? </Text>
+              <Text className="text-base font-semibold underline text-[#f49b33]">Sign in</Text>
+
+            </TouchableOpacity>
+
+          </View>
+        </View>
+
+        <View className=" flex-1">
+          <Image source={frame} className="w-full h-full " resizeMode="contain" />
+        </View>
+
+
+
+      </ScrollView>
+    </SafeAreaView>
   );
 }
