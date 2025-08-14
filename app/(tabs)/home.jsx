a3fc44af7fba48c357aa4a797d35c17c15f7754d
hafeezrafeeq <hafeezrafeeq031@gmail.com>
2025-08-14 23:51:03 +0500

    Enhance home screen layout and add restaurant data

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,21 +1,61 @@
-import { View, Text, Platform, Image } from 'react-native'
+import { View, Text, Platform, ImageBackground, ScrollView, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
+import { BlurView } from 'expo-blur';
 import { SafeAreaView } from 'react-native-safe-area-context'
+import { restaurants } from '../../store/restaurants';
+import { ActivityIndicator, FlatList } from 'react-native-web';
 
 const logo = require("../../assets/images/dinetimelogo.png")
-const bgImage = require("../../assets/images/homeBanner.png")
+const banner = require("../../assets/images/homeBanner.png")
+
+
+
 const home = () => {
+
+
+  const renderItem = ({item})=>(
+    <TouchableOpacity>
+      <Image resizeMode='cover' source={url.item.image}
+      className="h-28 mt-2 mb-2 rounded-lg"
+      ></Image>
+    </TouchableOpacity>
+)
   return (
     <SafeAreaView className="bg-[#2b2b2b]">
-      <View className="  rounded-lg shadow-lg justify-around items-center ">
-        <View className="flex flex-row bg-[#5f5f5f] w-11/12">
-          <Text className={`text-lg h-10 pt-[${Platform.OS == "ios" ? 8 : 6.5}] align-middle text-white`}>
-            Welcome ! {' '}
+      <View className="rounded-lg shadow-lg flex justify-center items-center ">
+        <View className="flex flex-row bg-[#5f5f5f]  p-6 rounded-lg w-11/12">
+          {/* <Image source={logo} resizeMode='cover' alt="image" style={{ width: 90, height:90  }} /> */}
+          <Text className={`text-2xl we  pt-[${Platform.OS == "ios" ? 8 : 6.5}] align-middle text-white`}>
+            Welcome to Dine Time !
           </Text>
-          <Image sourse={logo} resizeMode="cover" className="w-20 h-12" />
+
         </View>
       </View>
+
+
+      <ScrollView>
+        <ImageBackground
+          source={banner}
+          resizeMethod='cover'
+          className="w-full h-52 items-center   rounded-lg justify-center"
+        >
+
+          <BlurView intensity={Platform.OS === "android" ? 100 : 25}
+            tint="dark" className="w-full p-4 shadow-lg">
+            <Text className="text-center text-white text-3xl font-bold ">
+              Dine With Your Loved ones
+            </Text>
+          </BlurView>          I
+
+        </ImageBackground>
+      </ScrollView>
+
+      {restaurants.length > 0 ?
+        <FlatList deta={restaurants} renderTtems={renderItems} horizontal contentContainerStyle={{padding:16}} showsHorizontalScrollIndicator={false}  scrollEnabled={true}  /> : <ActivityIndicator animating color={"#fb9b33"} />
+
+      }
     </SafeAreaView>
+
   )
 }
 
